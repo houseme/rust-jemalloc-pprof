708cddae6ba7adf952bb0877c26d48d1f9ee9b1d
houseme <housemecn@gmail.com>
2026-06-03 11:39:30 +0800

    chore: drop rust-version metadata and satisfy clippy
    - remove rust-version metadata from workspace and member Cargo manifests
    - collapse the nested conditional in util::parse_jeheap to satisfy the current clippy lint set
    - keep the remaining staged manifest and parser cleanup together in one follow-up commit

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -414,39 +414,40 @@ pub fn parse_jeheap<R: BufRead>(
             addrs.reverse();
             cur_stack = Some(addrs);
         }
-        if words.len() > 2 && words[0] == "t*:" {
-            if let Some(addrs) = cur_stack.take() {
-                // The format here is e.g.:
-                // t*: 40274: 2822125696 [0: 0]
-                //
-                // "t*" means summary across all threads; someday we will support per-thread dumps but don't now.
-                // "40274" is the number of sampled allocations (`n_objs` here).
-                // On all released versions of jemalloc, "2822125696" is the total number of bytes in those allocations.
-                //
-                // To get the predicted number of total bytes from the sample, we need to un-bias it by following the logic in
-                // jeprof's `AdjustSamples`: https://github.com/jemalloc/jemalloc/blob/498f47e1ec83431426cdff256c23eceade41b4ef/bin/jeprof.in#L4064-L4074
-                //
-                // However, this algorithm is actually wrong: you actually need to unbias each sample _before_ you add them together, rather
-                // than adding them together first and then unbiasing the average allocation size. But the heap profile format in released versions of jemalloc
-                // does not give us access to each individual allocation, so this is the best we can do (and `jeprof` does the same).
-                //
-                // It usually seems to be at least close enough to being correct to be useful, but could be very wrong if for the same stack, there is a
-                // very large amount of variance in the amount of bytes allocated (e.g., if there is one allocation of 8 MB and 1,000,000 of 8 bytes)
-                //
-                // In the latest unreleased jemalloc sources from github, the issue is worked around by unbiasing the numbers for each sampled allocation,
-                // and then fudging them to maintain compatibility with jeprof's logic. So, once those are released and we start using them,
-                // this will become even more correct.
-                //
-                // For more details, see this doc: https://github.com/jemalloc/jemalloc/pull/1902
-                //
-                // And this gitter conversation between me (Brennan Vincent) and David Goldblatt: https://gitter.im/jemalloc/jemalloc?at=5f31b673811d3571b3bb9b6b
-                let n_objs: f64 = str::parse(words[1].trim_end_matches(':'))?;
-                let bytes_in_sampled_objs: f64 = str::parse(words[2])?;
-                let ratio = (bytes_in_sampled_objs / n_objs) / sampling_rate;
-                let scale_factor = 1.0 / (1.0 - (-ratio).exp());
-                let weight = bytes_in_sampled_objs * scale_factor;
-                profile.push_stack(WeightedStack { addrs, weight }, None);
-            }
+        if words.len() > 2
+            && words[0] == "t*:"
+            && let Some(addrs) = cur_stack.take()
+        {
+            // The format here is e.g.:
+            // t*: 40274: 2822125696 [0: 0]
+            //
+            // "t*" means summary across all threads; someday we will support per-thread dumps but don't now.
+            // "40274" is the number of sampled allocations (`n_objs` here).
+            // On all released versions of jemalloc, "2822125696" is the total number of bytes in those allocations.
+            //
+            // To get the predicted number of total bytes from the sample, we need to un-bias it by following the logic in
+            // jeprof's `AdjustSamples`: https://github.com/jemalloc/jemalloc/blob/498f47e1ec83431426cdff256c23eceade41b4ef/bin/jeprof.in#L4064-L4074
+            //
+            // However, this algorithm is actually wrong: you actually need to unbias each sample _before_ you add them together, rather
+            // than adding them together first and then unbiasing the average allocation size. But the heap profile format in released versions of jemalloc
+            // does not give us access to each individual allocation, so this is the best we can do (and `jeprof` does the same).
+            //
+            // It usually seems to be at least close enough to being correct to be useful, but could be very wrong if for the same stack, there is a
+            // very large amount of variance in the amount of bytes allocated (e.g., if there is one allocation of 8 MB and 1,000,000 of 8 bytes)
+            //
+            // In the latest unreleased jemalloc sources from github, the issue is worked around by unbiasing the numbers for each sampled allocation,
+            // and then fudging them to maintain compatibility with jeprof's logic. So, once those are released and we start using them,
+            // this will become even more correct.
+            //
+            // For more details, see this doc: https://github.com/jemalloc/jemalloc/pull/1902
+            //
+            // And this gitter conversation between me (Brennan Vincent) and David Goldblatt: https://gitter.im/jemalloc/jemalloc?at=5f31b673811d3571b3bb9b6b
+            let n_objs: f64 = str::parse(words[1].trim_end_matches(':'))?;
+            let bytes_in_sampled_objs: f64 = str::parse(words[2])?;
+            let ratio = (bytes_in_sampled_objs / n_objs) / sampling_rate;
+            let scale_factor = 1.0 / (1.0 - (-ratio).exp());
+            let weight = bytes_in_sampled_objs * scale_factor;
+            profile.push_stack(WeightedStack { addrs, weight }, None);
         }
     }
     if cur_stack.is_some() {
